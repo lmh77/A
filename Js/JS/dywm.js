15e882c09812a62248af477fea6c28c94622e25c
lmh77 <62322755+lmh77@users.noreply.github.com>
2021-01-24 09:43:38 +0800

    Add files via upload

---
+++ Js/JS/dywm.js
@@ -0,0 +1,6 @@
+//重写匹配地址:amemv.com/aweme/v1/aweme/post
+//QuanX重写配置:amemv.com/aweme/v1/aweme/post url script-response-body https://gitee.com/passerby-b/javascript/raw/master/dywm.js
+//MITM:*.amemv.com
+//需要到作者的作品列表里找到那个视频再下载
+
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(36)};if('0'.replace(0,e)==0){while(c--)r[e(c)]=k[c];k=[function(e){return r[e]||e}];e=function(){return'[2-9abdf-hj]'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('4.5("🍎抖音去水印脚本开始!");8 3=$response.3;d{f($request.url.indexOf("amemv.com/g/v1/g/post")>-1&&!!3){8 2=h.parse(3);for(8 i=0;i<2.6.length;i++){8 7=2.6[i].9.7.a;2.6[i].9.download_addr.a=7;f(!!2.6[i].9.j.a){2.6[i].9.j.a=7}4.5("🍎播放地址:"+7)}$b({3:h.stringify(2)})}else{4.5("🍎🍎3为空");$b({})}}catch(e){4.5("🍎🍎🍎🍎d错误:"+e);$b({})}4.5("执行完成!!!!");',[],20,'||obj|body|console|log|aweme_list|play_addr|var|video|url_list|done||try||if|aweme|JSON||download_suffix_logo_addr'.split('|'),0,{}))
