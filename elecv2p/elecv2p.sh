268048ea2a750f72ec71c6a456764685d975067d
lmh77 <lmhseven@qq.com>
2021-03-02 20:43:43 +0800

    update

--- elecv2p/elecv2p.sh
+++ elecv2p/elecv2p.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-#cron
-grep ^cron lxk0301_LoonTask.conf| cut -f2 -d\"|sed 's/[ \t]*$//g'|sed 's/^/"&/g'|sed 's/$/&"/g'>cron.list
-#脚本地址
-js=(`grep ^cron lxk0301_LoonTask.conf| cut -f2 -d= |sed 's/[tag\t]*$//g'|sed 's/[ \t]*$//g'|sed 's/[,\t]*$//g'`)
-#脚本名字
-name=(`grep ^cron lxk0301_LoonTask.conf| sed "s/.*tag=\(.*\)/\1/"`)
+#订阅名
+Subname=JDTask
+#需要转换的LOON订阅URL
+URL=https://jdsharedresourcescdn.azureedge.net/jdresource/lxk0301_LoonTask.conf
+#订阅描述
+SubDesc=elecv2p订阅
+#作者
+SubAuthor=lmh77
+
+
+#cron列表，临时生成cron.list用于读取，cron带星号不知道怎么处理筛选，最后执行删除
+curl -s $URL | grep ^cron | cut -f2 -d\"|sed 's/[ \t]*$//g'|sed 's/^/"&/g'|sed 's/$/&"/g'>cron.list
+#地址列表
+js=(`curl -s $URL | grep ^cron | cut -f2 -d= |sed 's/[tag\t]*$//g'|sed 's/[ \t]*$//g'|sed 's/[,\t]*$//g'`)
+#任务列表
+name=(`curl -s $URL | grep ^cron | sed "s/.*tag=\(.*\)/\1/"`)
 
 printf "{\n"
-printf '\t"name":"JD Task",\n'
-printf '\t"desc":"该订阅仅可用于 elecV2P, 与其他软件并不兼容。",\n'
-printf '\t"author":"lmh77",\n'
-printf '\t"date":"'$(date +%Y%m%d)'",\n'
-printf '\t"surl":"https://raw.githubusercontent.com/lmh77/A/main/elecv2p/elecv2p.json",\n'
+printf '\t"name":"'${Subname}'",\n'
+printf '\t"desc":"'${SubDesc}'",\n'
+printf '\t"author":"'${SubAuthor}'",\n'
+printf '\t"date":"'$(date +%Y/%m/%d__%H:%M:%S)'",\n'
+printf '\t"surl":"https://raw.githubusercontent.com/lmh77/A/main/elecv2p/lxk0301_cdn_elecv2p.json",\n'
 printf '\t"list":[\n'
 for ((i=0;i<${#name[@]};i++))
 do
