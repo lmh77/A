62eff7e6555a2be80c1c6ec582509f5113d88b8f
lmh77 <lmhseven@qq.com>
2021-03-03 12:39:47 +0800

    update

--- elecv2p/jd_local.sh
+++ elecv2p/jd_local.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#bash /usr/local/app/script/Shell/jd-local.sh>/usr/local/app/efss/lxk0301_local_elecv2p.json
 #订阅名
 Subname=JDTask
 #需要转换的LOON订阅URL
@@ -9,11 +9,11 @@ SubDesc=elecv2p订阅
 SubAuthor=lmh77
 
 #cron列表，临时生成cron.list用于读取，cron带星号不知道怎么处理筛选，最后执行删除
-curl -s $URL | grep ^cron | cut -f2 -d\"|sed 's/[ \t]*$//g'|sed 's/^/"&/g'|sed 's/$/&"/g'>cron.list
+grep ^cron /usr/local/app/script/JSFile/jd/Loon/lxk0301_LoonTask.conf| cut -f2 -d\"|sed 's/[ \t]*$//g'|sed 's/^/"&/g'|sed 's/$/&"/g'>cron.list
 #地址列表
-js=(`curl -s $URL | grep ^cron | cut -f2 -d= |sed 's/[tag\t]*$//g'|sed 's/[ \t]*$//g'|sed 's/[,\t]*$//g'|sed 's/https:\/\/jdsharedresourcescdn\.azureedge\.net\/jdresource\//jd\//g'`)
+js=(`grep ^cron /usr/local/app/script/JSFile/jd/Loon/lxk0301_LoonTask.conf| cut -f2 -d= |sed 's/[tag\t]*$//g'|sed 's/[ \t]*$//g'|sed 's/[,\t]*$//g'|sed 's/https:\/\/gitee.com\/lxk0301\/jd_scripts\/raw\/master\//jd\//g'`)
 #任务列表
-name=(`curl -s $URL | grep ^cron | sed "s/.*tag=\(.*\)/\1/"`)
+name=(`grep ^cron /usr/local/app/script/JSFile/jd/Loon/lxk0301_LoonTask.conf| sed "s/.*tag=\(.*\)/\1/"`)
 
 printf "{\n"
 printf '\t"name":"'${Subname}'",\n'
